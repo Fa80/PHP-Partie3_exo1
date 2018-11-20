7be2598b516a9317f0c2e9f92e438ee9a8be50bd
root <ngomfa@gmail.com>
2018-11-20 15:48:56 +0100

    exo-1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie3_Exo1</title>
+</head>
+<body>
+    <?php
+/*
+Créer une variable et l'initialiser à 0.
+Tant que cette variable n'atteint pas 10, il faut :
+
+    l'afficher
+    l'incrementer
+
+*/
+
+        $number = 0;
+          while($number <=10){
+              ?>
+         <p><?php echo $number++. ' ';
+          }
+   ?></p>
+</body>
+</html>
